ad07c66ca8a1c39a6471c238f4a69fa0880b8643
SMXaS <jurkisarnis@gmail.com>
2021-05-14 07:57:06 +0100

    virtual bot

---
+++ Virtual_Bot/bot.py
@@ -0,0 +1,28 @@
+""" Virtual Bot Text to Speech Convertions """
+from gtts import gTTS
+from playsound import playsound
+import os
+
+# Language of speech
+language = 'en'
+
+# Intro
+welcome_text = "Welcome to the Traffic Signs System"
+
+# End
+goodbye_text = "See you later, thank you for learning!"
+
+# Welcome text convertion
+output = gTTS(text=welcome_text, lang=language, slow=False)
+
+# Save welcome to audio
+output.save("welcome.mp3")
+
+# Goodbye text convertion
+output = gTTS(text=goodbye_text, lang=language, slow=False)
+
+# Save goodbye to audio
+output.save("goodbye.mp3")
+
+# Testing with audio play
+os.system("start welcome.mp3")
