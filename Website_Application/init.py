cf9223fe1ba3ebea4763274c14d9b8c6eba649cf
SMXaS <jurkisarnis@gmail.com>
2021-05-14 06:51:52 +0100

    camera

--- Website_Application/init.py
+++ Website_Application/init.py
@@ -1,15 +1,29 @@
+"""Links and Machine Learning Model connections"""
 from flask import Flask, render_template, url_for
 app = Flask(__name__)
+from keras.models import load_model
 
+# Importing model
+model = load_model("Saved_Model\model.h5")
+
+# Link to main page
 @app.route("/")
 @app.route("/main")
 def main():
     return render_template('main.html')
 
+# Link to info page
 @app.route('/')
 @app.route("/info")
 def info():
     return render_template('info.html')
+
+# Link to game page
+@app.route("/")
+@app.route("/game")
+def game():
+    return render_template('game.html')
+
 
 if __name__ == '__main__':
     app.run(debug=True)
